be24238b0589e70d0d7899eaddafa8013540285d
Mohd Sahil Ansari <125661601+mohd2548@users.noreply.github.com>
2025-12-25 18:55:59 +0530

    first Commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 5;
+    cout << (a+b) << endl;
+    cout <<"sum :" << (a+b) << endl;
+    cout <<"difference :" << (a-b) << endl;
+    cout <<"product :" << (a*b) << endl;
+    cout <<"Division :" << (a/b) << endl;
+    cout <<"modulo :" << (a%b) << endl;
+}
