620ed17a5148e95b7475b8fe8a64821b4a7d011f
Mohd Sahil Ansari <125661601+mohd2548@users.noreply.github.com>
2025-12-27 19:57:23 +0530

    Second commit for c++

--- problem.cpp
+++ problem.cpp
@@ -18,6 +18,14 @@ int main() {
     // cout << "Sum of a and b : " << (a + b) << endl;
     // improved 
     int sum = a + b;
+    int diff = a - b;
+    int prod = a * b;
+    int Div = a / b;
+    int mod = a % b;
     cout << "Sum of (a + b) : " << sum << endl;
+    cout << "Difference of (a - b) : " << diff << endl;
+    cout << "Product of (a * b) : " << prod << endl;
+    cout << "Division of (a / b) : " << Div << endl;
+    cout << "Modulo of (a % b) : " << mod << endl;
     return 0;
 }
