294ff2cd5389305c48f69d0e3e3079e7159adc53
Mohd Sahil Ansari <125661601+mohd2548@users.noreply.github.com>
2025-12-28 22:51:35 +0530

    Conditional statement and problems in C++

---
+++ Conditional_state.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+
+using namespace std;
+
+// int main() {
+//     int  n ;
+//     cout << "Enter the Number : ";
+//     cin >> n ;
+//     if (n > 0) {
+//         cout << " n is positive\n";
+//     }else{
+//         cout << "Please try again\n";
+//     }
+//     return 0;
+// }
+
+//  Taking user inout for age eligiblity to vote 
+
+// int main() {
+//     int age;
+//     cout << "Enter your age : ";
+//     cin >> age;
+//     if (age >= 18) {
+//         cout << "Your are eligible for vote \n";
+//     } else {
+//         cout << "You're a child not eligible\n";
+//     }
+//     return 0;
+// }
+
+//  Taking User input and find odd and even Number.
+
+// int main() {
+//     int n;
+//     cout << "Enter the number : ";
+//     cin >> n;
+//     if (n % 2 == 0) {
+//         cout << "Even Number\n";
+//     } else{
+//         cout << "Odd Number\n";
+//     }
+//     return 0;
+// }
+
+//  elseif  condition  grading sys
+
+// int main() {
+//     int marks;
+//     cout << "Enter your Marks : ";
+//     cin >> marks;
+//     if (marks >= 90) {
+//         cout << "Grade 'A' ";
+//     } else if(marks >= 80 && marks < 90){
+//         cout << "Grade 'B' ";
+//     } else if(marks < 80 && marks >= 50){
+//         cout << "Grade 'C' ";
+//     } else if(marks >= 35 && marks < 50) {
+//         cout << "Grade 'D' ";
+//     } else {
+//         cout << "Fail \n";
+//         cout << "Try again next year prepare well \n"; 
+//     }
+//     return 0;
+// }
+
+//  Find a char lowercase or upper case 
+
+// int main() {
+//     char ch ;
+//     cout << "Charachter please : ";
+//     cin >> ch;
+//     if(ch >= 'a' && ch <= 'z'){
+//         cout << "Lowercase char\n";
+//     } else {
+//         cout << "Uppercase char\n";
+//     }
+//     return 0;
+// }
+
+//  above problem by using Ascii number comaprision 
+//  [A -Z] -->  [65 - 90]  AND  [a - z]  -->  [97 - 122]
+
+// int main() {
+//     char ch ;
+//     cout << "Charachter please : ";
+//     cin >> ch;
+//     if(ch >= 65 && ch <= 90){
+//         cout << "Uppercase char\n";
+//     } else {
+//         cout << "Lowercase char\n";
+//     }
+//     return 0;
+// }
+
+//  printing  the single char into ASCII form
+
+// int main () {
+//     char ch ;
+//     cout << "Char : ";
+//     cin >> ch ;
+//     if(ch >= 0) {
+//         cout << "Ans ==> " <<(int)ch << endl;
+//     }else{
+//         cout << "Try again \n ";
+//     }
+//     return 0 ;
+// }
+
