e3f978a4720f79244be278acc6148d449f875e93
Mohd Sahil Ansari <125661601+mohd2548@users.noreply.github.com>
2026-01-06 01:25:09 +0530

    Patterns

---
+++ Patterns.cpp
@@ -0,0 +1,60 @@
+// Nested Loops with pattern problems 
+#include <iostream>
+using namespace std;
+/*
+1 2 3 4 
+1 2 3 4 
+1 2 3 4 
+1 2 3 4 
+*/
+
+// int main(){
+//     int n = 5;
+//     for(int i=1;i<n;i++){ // using i=0 then i<=n-1
+//         for(int j=1;j<n;j++){
+//             cout<<j << " ";
+//         }
+//     cout << endl;
+//     }
+//     return 0;
+// }
+
+// FOr characters
+
+/*
+A B C D 
+A B C D 
+A B C D 
+A B C D
+*/ 
+
+// int main(){
+//     int n = 4;
+//     for (int i=0;i<=n-1;i++){
+//         char ch = 'A';
+//         for(int j=0;j<=n-1;j++){
+//             cout << ch << " ";
+//             ch = ch+1;
+//         }
+//     cout << endl;
+//     }
+//     return 0;
+// }
+
+/*
+1 2 3 
+4 5 6 
+7 8 9 
+*/
+// int main(){
+//     int n =3;
+//     int num = 1;
+//     for (int i =0;i<n;i++){
+//         for(int j =0;j<n;j++){
+//             cout << num << " ";
+//             num ++;
+//         }
+//     cout << endl;
+//     }
+//     return 0;
+// }
