9c03e1cbbd17d0040526bf16525f1ee1f61dee74
Mohd Sahil Ansari <mohdsahila132@gmail.com>
2026-01-20 16:20:28 +0530

    Pattern

--- Patterns.cpp
+++ Patterns.cpp
@@ -82,15 +82,42 @@ A B C D
 // }
 
 
+// int main(){
+//     int n = 3;
+//     int num = 1;
+//     for (int i = 1; i<= n ; i ++){
+//         for (int j = 1; j<=n; j++){  
+//             cout << num << " ";         // num print the number in sequence by incrementing 
+//             num ++;
+//         }
+//     cout <<endl ;
+//     }
+//     cout << "After pattern num Value :"  << num << endl;
+//     return 0;
+// }
+
+
+// int main(){
+//     int n = 3;
+//     char ch = 'A';
+//     for (int i = 1; i<=n; i++){
+//         for (int j = 1; j<=n; j++){
+//             cout << ch ;
+//             ch ++;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+// Triangle pattern +
 int main(){
-    int n = 4;
-    int num = 1;
-    for (int i = 1; i<= n ; i ++){
-        for (int j = 1; j<=n; j++){  
-            cout << num << " ";         // num print the number in sequence by incrementing 
-            num ++;
+    int n = 6;
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j<= i; j++){
+            cout << "*";
         }
-    cout << endl;
+        cout << endl;
     }
     return 0;
 }
